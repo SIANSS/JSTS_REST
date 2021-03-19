37baa5a6a2b8ad499332731d9f81e23ac7099300
SIANSS <acsian7@gmail.com>
2021-03-19 12:18:10 +0530

    terraformed

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+variable "shared_cred_file" {
+ default = "/home/dimosian/.aws/credentials"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+  profile = "default"
+  shared_credentials_file = "${var.shared_cred_file}"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  key_name = "mykey"
+
+  tags = {
+    Name = "terraform Test Instance"
+  }
+}
